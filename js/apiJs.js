3c535c45a4c1239e2114796e24fdd50411b09fae
Tadas.daugalas <tadas.daugalas1@gmail.com>
2021-11-02 17:58:36 +0200

    Add midlle filter , add cart

--- js/apiJs.js
+++ js/apiJs.js
@@ -15,8 +15,9 @@ $(function () {
     function errorHandler(responese) {
         console.log(responese.status);
     }
-    $.fn.stars = function() {
-        return $(this).each(function() {
+
+    $.fn.stars = function () {
+        return $(this).each(function () {
             // Get the value
             var val = parseFloat($(this).html());
             // Make sure that the value is in 0 - 5 range, multiply to get width
@@ -27,37 +28,68 @@ $(function () {
             $(this).html($span);
         });
     }
+    $('select').change(function () {
+        const select = $('#selectCatBlack').val();
+        if (select !== "All Category") {
+            let fillArrayWIthSelect = products.filter(p => p.category === select);
+            fillTable(fillArrayWIthSelect);
+        } else {
+            //Fill all table
+        }
+    }).change();
     $('#search').click(function () {
         // const search = parseFloat($('#searchText').val());
         // let filteredArray = products.filter(p => p.price === search);
-        const search = $('#searchText').val();
-        let fillterArray = products.filter(p => p.title.includes(search)||p.description.includes(search)||p.category.includes(search));
-        fillTable(fillterArray);
+        const select = $('#selectCat').val();
+        if (select === "All Category") {
+            const search = $('#searchText').val();
+            let fillterArray = products.filter(p => p.title.includes(search) || p.description.includes(search) || p.category.includes(search));
+            fillTable(fillterArray);
+        } else {
+            const search = $('#searchText').val();
+            let fillArrayWIthSelect = products.filter(p => p.category === select && (p.title.includes(search) || p.description.includes(search)));
+            fillTable(fillArrayWIthSelect);
+        }
     })
-    function fillTable(array){
-        if(array.length >0){
+    let counterVal = 0;
+
+    function incrementClick() {
+        updateDisplay(++counterVal);
+    }
+    function updateDisplay(val) {
+        document.getElementById("display").innerHTML = val;
+    }
+
+
+    function fillTable(array) {
+        if (array.length > 0) {
             $('#serv').removeClass('invisible');
             let rows = '';
-            array.forEach(function(item){
-                rows += '<li>'+
-                    '<div class="card align-items-center" style="width:auto;">'+
-                        '<img src="'+item.image+'" class="card-img image-size"'+
-                             'alt="...">'+
-                            '<div class="card-body w-100">'+
-                                '<h5 class="card-title text-elipsis">'+item.title+'</h5>'+
-                                '<span class="card-text fs-5">'+"Ratio:"+item.rating.rate+'</span>'+
-                                '<p class="card-text fs-5">'+"$"+item.price+'</p>'+
-                            '</div>'+
-                    '</div>'+
-                '</li>'
+
+            array.forEach(function (item) {
+                rows += '<li>' +
+                    '<div class="card align-items-center" style="width:auto;">' +
+                    '<img src="' + item.image + '" class="card-img image-size"' +
+                    'alt="...">' +
+                    '<div class="card-body w-100">' +
+                    '<h5 class="card-title text-elipsis">' + item.title + '</h5>' +
+                    '<span class="stars mb-1 card-text fs-5">' + "Ratio:" + item.rating.rate + '</span>' +
+                    '<p class="card-text fs-5">' + "$" + item.price +
+                    ' <button id="buyButton" type="button" onclick="incrementClick()" class="btn">\n' +
+                    '                    <i class="fas fa-shopping-bag"></i>' + '</p>' +
+                    '</div>' +
+                    '</div>' +
+                    '</li>'
             });
             $(' ul').html(rows);
-        }else {
-        alert("Rezults not found")
+        } else {
+            alert("Rezults not found")
         }
     }
-    $(function() {
+
+    $(function () {
         $('span.stars').stars();
     });
 
-});
+})
+;
